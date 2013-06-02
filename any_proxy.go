ec3d38040c696cc7f50208ea13045e18508f0e8d
Ryan A. Chapman <ryan@rchapman.org>
2013-06-01 21:32:23 -0600

    Add gitignore, fix comment that was referencing wrong ip address

--- any_proxy.go
+++ any_proxy.go
@@ -300,7 +300,7 @@ func getOriginalDst(clientConn *net.TCPConn) (ipv4 string, port uint16, newTCPCo
     // this is the only syscall in the Golang libs that I can find that returns 16 bytes
     // Example result: &{Multiaddr:[2 0 31 144 206 190 36 45 0 0 0 0 0 0 0 0] Interface:0}
     // port starts at the 3rd byte and is 2 bytes long (31 144 = port 8080)
-    // IPv4 address starts at the 5th byte, 4 bytes long (74 125 225 177)
+    // IPv4 address starts at the 5th byte, 4 bytes long (206 190 36 45)
     addr, err :=  syscall.GetsockoptIPv6Mreq(int(clientConnFile.Fd()), syscall.IPPROTO_IP, SO_ORIGINAL_DST)
     log.Debugf("getOriginalDst(): SO_ORIGINAL_DST=%+v\n", addr)
     if err != nil {
