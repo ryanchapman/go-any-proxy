56ad843a4b53e02982c6e70d6f677d6462e55b6c
Zack Dannar <zack.dannar@rightnow.com>
2013-11-05 16:37:25 -0700

    Removed debugging statements

--- any_proxy.go
+++ any_proxy.go
@@ -150,7 +150,6 @@ func buildDirectors(gDirects string) ([]directorFunc) {
     for idx,dstrOrig := range dstrings {
         dstring := dstrOrig
         var dfunc directorFunc
-        log.Debugf("ZEBUG : Building director for %s", dstring)
         if strings.Contains(dstring, "/") {
 
             cidrIp, cidrIpNet, err := net.ParseCIDR(dstring)
@@ -177,7 +176,6 @@ func buildDirectors(gDirects string) ([]directorFunc) {
         }
 
     }
-    log.Debugf("ZEBUG : Showing directors : %+v", directors)
     return directors
 }
 
@@ -579,7 +577,6 @@ func handleConnection(clientConn *net.TCPConn) {
         return
     }
     // Evaluate for direct connection
-    log.Debugf("ZEBUG : ipv4 is %s, %+v", ipv4, ipv4)
     if ok,_ := director(ipv4); ok {
             handleDirectConnection(clientConn, ipv4, port)
             return
